4ba9bafad71951324849c3952769c0477cfc5217
mattr21 <matt.rothstein21@gmail.com>
2019-02-25 14:45:58 -0800

    setup Routes for / (home), /about, and /contact in App

--- src/index.js
+++ src/index.js
@@ -6,4 +6,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById('root')
+);
