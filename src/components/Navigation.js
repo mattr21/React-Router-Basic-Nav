ee5bc589b2f898c9b53e9d0943d567aeba21cf45
mattr21 <matt.rothstein21@gmail.com>
2019-02-25 15:17:12 -0800

    MVP complete

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,7 +10,7 @@ const Navigation = () => {
           <Link to="/">Home</Link>
         </div>
         <div>
-          <Link to="/about">About</Link>
+          <Link to="/about">About</Link> 
         </div>
         <div>
           <Link to="/contact">Contact</Link>
